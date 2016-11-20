5e8f88c473005a8b1a0f41b8513eff4f5dce1142
shipperizer <alexcabb@gmail.com>
2016-11-20 21:03:14 +0000

    [++] use api tutorial

---
+++ todos/web/router.ex
@@ -0,0 +1,25 @@
+defmodule Todos.Router do
+  use Todos.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", Todos do
+    pipe_through :api
+
+    resources "/todos", TodoController, only: [:index, :show]
+  end
+  # Other scopes may use custom stacks.
+  # scope "/api", Todos do
+  #   pipe_through :api
+  # end
+end
